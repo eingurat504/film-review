64f762e5f2d16b97c145fa241cf11a63028e9f6b
emma ingurat <e.ingurat@bsmart-technology.com>
2021-05-28 15:05:25 +0300

    Added Upcoming movie

---
+++ src/views/movie/Upcoming.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <ul>
+	  <li v-for="movie in movies">
+	    {{ movie.original_title }}
+	  </li>
+	</ul>
+  </div>
+</template>
+<script>
+	import axios from 'axios'
+export default {
+  name: 'Upcoming',
+  data() {
+
+  	return {
+  		movies: []
+  	}
+   
+  },
+  created() {
+     if (!this.movies.length) {
+            this.getUpcomingMovies();
+        } 
+  },
+  methods: {
+
+  	getUpcomingMovies(){
+
+	  	axios.get('/movie/upcoming?api_key=' + process.env.VUE_APP_API_TOKEN)
+	  	.then(response => {
+	  		console.log(response.data.results);
+	  		this.movies = response.data.results;
+	  		// movies: response.data.results
+	  	}).catch(error => {
+	  		console.log(error);
+	  	});
+
+  	}
+
+  }
+}
+</script>
