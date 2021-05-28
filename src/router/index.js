68023cfb1722327d27494991a39b76af05a833c9
emma ingurat <e.ingurat@bsmart-technology.com>
2021-05-28 15:21:24 +0300

    Added Toprated movie

--- src/router/index.js
+++ src/router/index.js
@@ -25,6 +25,14 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/movie/Upcoming.vue')
+  },
+  {
+    path: '/top_rated',
+    name: 'Toprated',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/movie/Top_rated.vue')
   }
 ]
 
---
+++ src/views/movie/Top_rated.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <ul>
+	  <li v-for="movie in movies">
+	    {{ movie.original_title }}
+	  </li>
+	</ul>
+  </div>
+</template>
+<script>
+	import axios from 'axios'
+export default {
+  name: 'Toprated',
+  data() {
+
+  	return {
+  		movies: []
+  	}
+   
+  },
+  created() {
+     if (!this.movies.length) {
+            this.getTopratedMovies();
+        } 
+  },
+  methods: {
+
+  	getTopratedMovies(){
+
+	  	axios.get('/movie/top_rated?api_key=' + process.env.VUE_APP_API_TOKEN)
+	  	.then(response => {
+	  		console.log(response.data.results);
+	  		this.movies = response.data.results;
+	  		// movies: response.data.results
+	  	}).catch(error => {
+	  		console.log(error);
+	  	});
+
+  	}
+
+  }
+}
+</script>
